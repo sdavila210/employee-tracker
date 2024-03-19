f74b6c17adad2982ca7772a159a3352e6384eba9
sarahd <sarahjdavila210@gmail.com>
2024-03-18 23:35:48 -0700

    fixed & updated schema.sql & seeds.sql

--- db/schema.sql
+++ db/schema.sql
@@ -11,14 +11,14 @@ CREATE TABLE department (
 CREATE TABLE role (
   id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
   title VARCHAR(30) NOT NULL,
-  salary DECIMAL NOT NULL
-  department_id: INT NOT NULL
+  salary DECIMAL NOT NULL,
+  department_id INT NOT NULL
 );
 
 CREATE TABLE employee (
-    id: INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     first_name VARCHAR(30),
     last_name VARCHAR(30),
     role_id INT NOT NULL,
     manager_id INT
-)
+);
